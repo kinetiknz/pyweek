0fade0d9ab6d2164ba94271e54fa167572d56d8a
xar <xar@orcon.net.nz>
2006-09-05 23:23:48 +0000

    Reverted last change. Tile collisions not quite fixed. Added movement.py and added collision between enemies and background.

--- hci/game.py
+++ hci/game.py
@@ -237,7 +237,7 @@ class Bullet(Sprite):
 class Human(Sprite):
     def __init__(self, game, tile, values=None):
         super(Human, self).__init__('enemy', 'enemy', game, tile, values)
-        self.sprite.agroups = game.string2groups('player')
+        self.sprite.agroups = game.string2groups('Background')
         self.sprite.hit = lambda game, sprite, other: self.hit(game, sprite, other)
 
     def step(self, game, sprite):
@@ -252,7 +252,7 @@ class Human(Sprite):
         self.sprite.rect.y = myloc[1]
 
     def hit(self, game, sprite, other):
-        pass
+        push(sprite, other)
 
 class Saucer(Sprite):
     def __init__(self, game, tile, values=None):
@@ -335,8 +335,6 @@ cdata = {
 tdata = {
     0x02: ('player', tile_block,
            {'top': 1, 'bottom': 1, 'left': 1, 'right': 1}),
-    0x02: ('enemy', tile_block,
-           {'top': 1, 'bottom': 1, 'left': 1, 'right': 1}),           
     0x20: ('player', tile_coin, None),
     0x30: ('player', tile_fire, None),
     }
