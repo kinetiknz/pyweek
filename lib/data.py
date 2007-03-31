2e9efe2ab8e6d7328a8485499b96ca43bbd6f86e
Matthew Gregan <kinetik@flim.org>
2007-03-31 22:18:03 +0000

    update to skellington 1.6.

--- lib/data.py
+++ lib/data.py
@@ -10,6 +10,11 @@ import os
 data_py = os.path.abspath(os.path.dirname(__file__))
 data_dir = os.path.normpath(os.path.join(data_py, '..', 'data'))
 
+def filepath(filename):
+    '''Determine the path to a file in the data directory.
+    '''
+    return os.path.join(data_dir, filename)
+
 def load(filename, mode='rb'):
     '''Open a file in the data directory.
 
--- run_game.py
+++ run_game.py
@@ -2,8 +2,12 @@
 
 import sys
 import os
-libdir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'lib'))
-sys.path.insert(0, libdir)
+try:
+    libdir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'lib'))
+    sys.path.insert(0, libdir)
+except:
+    # probably running inside py2exe which doesn't set __file__
+    pass
 
 import main
 main.main()
