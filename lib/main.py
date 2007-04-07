e845cd1a8c7a6b74ca57d7ed3a0097b2d7e34b0c
Matthew Gregan <kinetik@flim.org>
2007-04-07 06:45:27 +0000

    rearrange main.main to deal with different game states.

--- lib/main.py
+++ lib/main.py
@@ -39,63 +39,119 @@ def add_a_balloon(base_pos, lvl):
     new.position = base_pos + euclid.Vector2(random.randrange(250.0, 450.0), random.randrange(0.0, 150.0))
     display.sprite_list.append(new)
 
+class SplashRunner(object):
+    def run(self):
+        pass
+
+class MenuRunner(object):
+    def __init__(self, levels):
+        self.levels = levels
+
+    def run(self):
+        '''Display the game menu.  Return an index into self.levels to indicate where to start from.'''
+        return 0
+
+class GameEndRunner(object):
+    def __init__(self, win):
+        self.win = win
+
+    def run(self):
+        '''Display a win or lose graphic depending on self.win.  Return 2 to try
+           playing again from the current level, 1 to return to the menu, and 0
+           to quit the game.'''
+        return 0
+
+class LevelRunner(object):
+    def __init__(self, name):
+        self.name = name
+
+    def run(self):
+        '''Return True to indicate the level was beaten, False to indicate the player died.'''
+        lvl = level.load_level(self.name)
+
+        stick_guy      = player.Player(lvl, display.sprite_list)
+        display.player = stick_guy
+        display.lvl    = lvl
+
+        display.sprite_list.append(stick_guy)
+
+        # TODO: this should be set up by the level loader
+        emit = sprite.Emitter(lvl, display.sprite_list)
+        emit.position = stick_guy.position + euclid.Vector2(300.0, 200.0)
+        display.sprite_list.append(emit)
+
+        # TODO: this should be set up by the level loader
+        launch = sprite.DartLauncher(lvl, display.sprite_list)
+        launch.position = stick_guy.position + euclid.Vector2(0.0, 0.0)
+        display.sprite_list.append(launch)
+
+        # TODO: this should be set up by the level loader
+        for i in xrange(0,20):
+            add_a_balloon(stick_guy.position, lvl)
+
+        timer = pygame.time.Clock()
+
+        fps_limit = 1.0/60.0
+
+        while 1:
+            for e in pygame.event.get():
+                if e.type == pygame.QUIT or \
+                        e.type == pygame.KEYDOWN and \
+                        e.key == pygame.K_ESCAPE:
+                    sys.exit(0)
+
+                if e.type == pygame.KEYUP:
+                    if e.key == pygame.K_UP:
+                        stick_guy.add_balloon()
+                    elif e.key == pygame.K_DOWN:
+                        stick_guy.drop_balloon()
+
+            elapsed = timer.tick() / 1000.0
+
+            # why not just use timer.tick(fps)?  good question.  it seems to eat
+            # a lot more CPU than seems reasonable, almost as if it uses
+            # pygame.time.delay rather than pygame.time.wait... but! the docs
+            # say that only timer.tick_busy_loop() does this.  *shrug*
+            if elapsed < fps_limit:
+                dt = int((fps_limit - elapsed) * 1000)
+                pygame.time.wait(dt)
+
+            keys = pygame.key.get_pressed()
+
+            if (keys[pygame.K_LEFT]):
+                stick_guy.move_left()
+
+            if (keys[pygame.K_RIGHT]):
+                stick_guy.move_right()
+
+            display.update(elapsed)
+
 def main():
-    lvl = level.load_level("level_01")
+    levels = [LevelRunner("level_01"), LevelRunner("t001")]
 
-    stick_guy      = player.Player(lvl, display.sprite_list)
-    display.player = stick_guy
-    display.lvl    = lvl
+    splash = SplashRunner()
+    menu = MenuRunner(levels)
 
+    splash.run()
 
-    display.sprite_list.append(stick_guy)
+    play = 1
+    lastlevel = -1
 
-    emit = sprite.Emitter(lvl, display.sprite_list)
-    emit.position = stick_guy.position + euclid.Vector2(300.0, 200.0)
-    display.sprite_list.append(emit)
+    while play > 0:
+        if play == 1:
+            picked = menu.run()
+            to_play = levels[picked:]
+        else:
+            to_play = levels[lastlevel:]
 
-    launch = sprite.DartLauncher(lvl, display.sprite_list)
-    launch.position = stick_guy.position + euclid.Vector2(0.0, 0.0)
-    display.sprite_list.append(launch)
+        # play from the selected level onwards...
+        for level in to_play:
+            display.sprite_list = []
+            win = level.run()
+            if win:
+                lastlevel += 1
+            else:
+                break
 
-
-
-    for i in xrange(0,20):
-        add_a_balloon(stick_guy.position, lvl)
-
-
-    timer = pygame.time.Clock()
-
-    fps_limit = 1.0/60.0
-
-    while 1:
-        for e in pygame.event.get():
-            if e.type == pygame.QUIT or \
-                    e.type == pygame.KEYDOWN and \
-                    e.key == pygame.K_ESCAPE:
-                sys.exit(0)
-
-            if e.type == pygame.KEYUP:
-                if e.key == pygame.K_UP:
-                    stick_guy.add_balloon()
-                if e.key == pygame.K_DOWN:
-                    stick_guy.drop_balloon()
-
-        elapsed = timer.tick() / 1000.0
-
-        # why not just use timer.tick(fps)?  good question.  it seems to eat
-        # a lot more CPU than seems reasonable, almost as if it uses
-        # pygame.time.delay rather than pygame.time.wait... but! the docs
-        # say that only timer.tick_busy_loop() does this.  *shrug*
-        if elapsed < fps_limit:
-            dt = int((fps_limit - elapsed) * 1000)
-            pygame.time.wait(dt)
-
-        keys = pygame.key.get_pressed()
-
-        if (keys[pygame.K_LEFT]):
-            stick_guy.move_left()
-
-        if (keys[pygame.K_RIGHT]):
-            stick_guy.move_right()
-
-        display.update(elapsed)
+        gameend = GameEndRunner(win)
+        play = gameend.run()
